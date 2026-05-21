f791c8536dcced8167cb7093b3ef8f5ccab7c8f1
Denis <gureevdopp@mail.ru>
2026-05-21 09:58:21 +0300

    Add an entry point in the main

--- src/main/java/ru/psu/coursework/server/main/Main.java
+++ src/main/java/ru/psu/coursework/server/main/Main.java
@@ -1,5 +1,9 @@
 package ru.psu.coursework.server.main;
 
+import ru.psu.coursework.server.database.DatabaseManager;
+import ru.psu.coursework.server.network.LobbyManager;
+import ru.psu.coursework.server.network.PlayerConnection;
+
 import java.net.*;
 import java.io.*;
 
@@ -7,12 +11,24 @@ public class Main {
     private static final int PORT = 8765;
 
     public static void main(String[] args) throws IOException {
+        System.out.println("Starting server...");
+
+        DatabaseManager database = new DatabaseManager();
+        LobbyManager lobby = new LobbyManager();
 
         ServerSocket serverSocket = new ServerSocket(PORT);
 
         try {
+            System.out.println("Server successfully started and listening on port " + PORT);
+
             while (true) {
                 Socket socket = serverSocket.accept();
+
+                PlayerConnection connection = new PlayerConnection(socket, database, lobby);
+
+                Thread thread = new Thread(connection);
+                thread.start();
+
 
             }
         } finally {
